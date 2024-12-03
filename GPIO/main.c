2adf1f51f994a706e7d1a5df8b07ad73f0d1637d
Hao Nguyen <haonp1999@gmail.com>
2024-12-04 00:42:08 +0700

    Ok

---
+++ GPIO/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    // Người tạo: Xuân Thức
+    int num;
+    num = 10;
+    num = num + 5;  // cộng thêm 5 đơn vị
+    printf("num = %d\n",num);  // in ra màn hình 
+}
